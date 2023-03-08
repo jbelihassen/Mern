cc81fc3aa5b7b54644fb64350b02cca27862fd38
John Doe <johndoe@example.com>
2023-03-08 15:52:07 +0100

    First React with JSX

---
+++ First React with JSX/src/App.js
@@ -0,0 +1,18 @@
+import React from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Hello Dojo! </h1>
+     <h4>  Things I need to do:</h4>
+     <li>Learn React</li>
+     <li> Climb Mt.Everest </li>
+     <li> Run a marathon </li>
+     <li> Feed the dogs </li>
+     
+    </div>
+  );
+}
+
+export default App;
